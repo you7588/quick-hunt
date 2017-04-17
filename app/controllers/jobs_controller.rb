429007e9154dd29d5101a8a74b9dce44f63d6433
anndoko <handcuffanndo@gmail.com>
2017-04-18 01:16:14 +0800

    user cannot access hidden jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -9,6 +9,11 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
+
+    if @job.is_hidden
+      flash[:warning] = "This Job has been archived already."
+      redirect_to root_path
+    end
   end
 
   def new
