1a657d7845c87c4cb8e5ace556ace4efa8f4a809
anndoko <handcuffanndo@gmail.com>
2017-04-19 02:18:37 +0800

    use seeds.rb to generate data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# automatically create an  account
+create_account = User.create([email: 'anndo@rails.com', password: '111111', password_confirmation: '111111', is_admin: 'false'])
+puts "1 non-admin account created."
+
+# automatically create an admin account
+create_account = User.create([email: 'admin@rails.com', password: '111111', password_confirmation: '111111', is_admin: 'true'])
+puts "1 admin account created."
+
+# automatically create public jobs
+create_jos = for i in 1..10 do
+  Job.create!([title: "Job no.#{i}", description: "#{i} Job", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, contact_email: 'anndo@rails.com', is_hidden: "false"])
+end
+puts "10 public jobs created."
+
+# automatically create hidden jobs
+create_jos = for i in 1..10 do
+  Job.create!([title: "Job no.#{i+10}", description: "#{i+10} hidden job", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, contact_email: 'anndo@rails.com', is_hidden: "true"])
+end
+
+puts "10 hidden jobs created."
