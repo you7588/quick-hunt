c6fcce935c3745a7c9c33e5efe7aa4950663df8b
anndoko <handcuffanndo@gmail.com>
2017-04-24 22:42:10 +0800

    implement language switch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
+
   def require_is_admin
     if !current_user.admin?
       flash[:alert] = 'You are not admin'
