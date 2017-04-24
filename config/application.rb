4a143ba881326a0a4c9efc9f68cdac1b31fb848e
anndoko <handcuffanndo@gmail.com>
2017-04-25 00:27:25 +0800

    set defult language as en

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module JobListingContest
   class Application < Rails::Application
-    config.i18n.default_locale = "zh-CN"
+    config.i18n.default_locale = "en"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
