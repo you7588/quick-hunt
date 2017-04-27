ff62c6cc68a8ea4fa97e38847b6b6ce1436c63df
anndoko <handcuffanndo@gmail.com>
2017-04-27 23:44:26 +0800

    star or unstar a job

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,26 @@ class User < ApplicationRecord
   has_many :star_relationships
   has_many :starred_jobs, :through => :star_relationships, :source => :job
 
+
+
   def admin?
     is_admin
   end
 
+  # if user has starred the job
   def is_follower_of?(job)
     starred_jobs.include?(job)
   end
+
+  #star a job
+  def star!(job)
+    starred_jobs << job
+  end
+
+  #unstar a job
+  def unstar!(job)
+    starred_jobs.delete(job)
+  end
+
 
 end
